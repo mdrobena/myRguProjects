a95fae95c8bc18f9115e5db024cea98414b6b8d5
1511989 <michal.drobena.11@aberdeen.ac.uk>
2016-03-08 10:16:57 +0000

    b

---
+++ Week06/loop.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1511989
+ * Date: 08/03/2016
+ * Time: 10:04
+ */
+for($x = 1; $x < 31; $x++){
+    if($x = 1){
+        print "On the ".$x. "st of the month no products are available.";
+    }
+    else if($x % 2 == 0){
+        print "On the ".$x. "th of the month specs are available.";
+    }
+    else if($x % 3 == 0){
+        print "On the ".$x. "th of the mugs specs are available.";
+    }
+    else if($x % 4 == 0){
+        print "On the ".$x. "th of the month sausage rolls are available.";
+    }
+}
