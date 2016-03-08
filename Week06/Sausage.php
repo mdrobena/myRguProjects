ae26f0d70dcd5d3b42d20b83207bd5e458c7a303
1511989 <michal.drobena.11@aberdeen.ac.uk>
2016-03-08 09:17:42 +0000

    b

--- Week06/Sausage.php
+++ Week06/Sausage.php
@@ -1,14 +1,16 @@
 <?php
-    if($age > 16 && $age < 18){
+    $age = $_GET["$age"];
+
+    if($age >=16){
         echo "Specs.";
     }
-    else if ($age > 18 && $age < 21){
-        echo "Mugs.";
+    else if ($age >=18){
+        echo "Mugs and Specs";
     }
-    else if ($age > 21){
-        echo "Mugs.";
+    else if ($age >=21){
+        echo "Mugs, Rolls and Specs";
     }
     else{echo "Too young!";}
-
+?>
 
 
