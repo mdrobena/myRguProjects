1de369c8c27a104898dcc6d32e6bcff66cb0f99a
1511989 <michal.drobena.11@aberdeen.ac.uk>
2016-03-14 10:46:39 +0000

    b

---
+++ Week07/home.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1511989
+ * Date: 14/03/2016
+ * Time: 10:43
+ */
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<h1>Hello</h1>
+</body>
+</html>
+?>
---
+++ Week07/login.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1511989
+ * Date: 14/03/2016
+ * Time: 10:13
+ */
+include("dbconnect.php");
+
+if(empty($_POST["username"]) || empty($_POST["password"])){
+    echo "Both field are required.";
+}
+else{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+
+    $sql = "SELECT uid FROM users WHERE username = '$username' and password = '$password'";
+
+    $result = mysqli_query($db, $sql);
+
+    if(mysqli_num_rows($result) == 1){
+        header("location: home.php");
+    }
+    else{
+        echo "Incorrect username or password.";
+    }
+
+}
+?>
