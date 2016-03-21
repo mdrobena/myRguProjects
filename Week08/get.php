a8f27c72c80b81b15b58772a7066c64c5c2e29f9
1511989 <michal.drobena.11@aberdeen.ac.uk>
2016-03-21 10:45:45 +0000

    b

--- Week08/get.php
+++ Week08/get.php
@@ -5,7 +5,7 @@
     <title>Hello!</title>
 </head>
 <body>
-    <a href="http://nothingworks1.azurewebsites.net/Week08/get.php?w=world&p=printers&s=scanners">Click me!</>
+    <a href="http://nothingworks1.azurewebsites.net/Week08/get.php?w=world&p=printers&s=scanners">Click me!<br></>
     <?php
     $w = $_GET["w"];
     $p = $_GET["p"];
