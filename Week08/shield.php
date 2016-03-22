99b38547ff3c7a375a2c2aafc750104db1067c6d
1511989 <michal.drobena.11@aberdeen.ac.uk>
2016-03-22 10:51:08 +0000

    b

---
+++ Week08/shield.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1511989
+ * Date: 22/03/2016
+ * Time: 10:40
+ */
+echo $_POST["fname"]." ".$_POST["lname"]." ,".$_POST["gender"]."<br>";
+echo "Superpowers: ".$_POST["power"];
