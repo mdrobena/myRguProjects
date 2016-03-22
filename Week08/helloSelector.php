5b50166ad34a34e1a0597221e4eeefabfc6ddb4b
1511989 <michal.drobena.11@aberdeen.ac.uk>
2016-03-22 09:11:24 +0000

    b

---
+++ Week08/helloSelector.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Selector</title>
+</head>
+<body>
+    <h2>Where would you like to say hello?<br></h2>
+    <a href="helloSelector.php&v=Earth">Earth</>
+    <a href="helloSelector.php&v=Mars">Mars</>
+    <a href="helloSelector.php&v=Uranus">Uranus</>
+<body>
+</html>
