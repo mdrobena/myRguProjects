95632793088f3861f5adf889b8cb6546a086a232
1511989 <michal.drobena.11@aberdeen.ac.uk>
2016-03-22 10:19:14 +0000

    b

--- Week07/Marvel Movies/marvelStudio.php
+++ Week07/Marvel Movies/marvelStudio.php
@@ -1,7 +1,34 @@
-<?php
+<!DOCTYPE html>
 /**
- * Created by PhpStorm.
- * User: 1511989
- * Date: 22/03/2016
- * Time: 09:40
- */
+* Created by PhpStorm.
+* User: 1511989
+* Date: 14/03/2016
+* Time: 10:43
+*/
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>All Movies</title>
+    <!--<link rel="stylesheet" href="style.css" type="text/css"/-->
+</head>
+<body>
+<h1>Movies in the marvelmovies db:</h1>
+
+<?php
+include("dbconnect.php");
+$sql = "SELECT title FROM marvelmovies WHERE productionStudio = 'Marvel Studios'";
+$result = mysqli_query($db, $sql);
+
+if(mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["marvelMovieID"] . " " . $row["title"] . "<br>";
+    }
+}
+else{
+    echo "0 results";
+}
+
+?>
+
+</body>
+</html>
