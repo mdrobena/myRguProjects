d09afd56396a5b1fbfab19aa2b1f260e264added
1511989 <michal.drobena.11@aberdeen.ac.uk>
2016-03-22 09:55:56 +0000

    b

---
+++ Week07/Marvel Movies/allMovies.php
@@ -0,0 +1,37 @@
+<?php
+session_start()
+?>
+<!DOCTYPE html>
+/**
+* Created by PhpStorm.
+* User: 1511989
+* Date: 14/03/2016
+* Time: 10:43
+*/
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>All Movies</title>
+    <!--<link rel="stylesheet" href="style.css" type="text/css"/-->
+</head>
+<body>
+<h1>Movies in the marvelmovies db:</h1>
+
+<?php
+include("dbconnect.php");
+$sql = "SELECT title FROM marvelmovies";
+$result = mysqli_query($db, $sql);
+
+if(mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["marvelMovieID"] . " " . $row["title"] . "<br>";
+    }
+}
+else{
+    echo "0 results";
+}
+
+?>
+
+</body>
+</html>
