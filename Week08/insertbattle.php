de32b2e3f8efe4b911f8f8a6651abc7198d24601
mdrobena <mdrobena@rgu.ac.uk>
2016-04-07 18:38:43 +0100

    b

---
+++ Week08/insertbattle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zdeno
+ * Date: 07/04/2016
+ * Time: 18:02
+ */
+include ("dbconnect.php");
+
+$firstName = $_POST['$firstName'];
+$lastName = $_POST['$lastName'];
+$villan = $_POST['villan'];
+
+
+?>
