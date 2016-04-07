9549566993f32ebab3c46d370c65a37a87177d29
mdrobena <mdrobena@rgu.ac.uk>
2016-04-07 16:42:08 +0100

    b

---
+++ Week08/showsuperheros.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zdeno
+ * Date: 07/04/2016
+ * Time: 14:15
+ */
+include ("dbconnect.php");
+
+$sql_query = "SELECT * FROM superheros";
+$result = $db->query($sql_query);
+while($row = $result->fetch_array()){
+    $firstName = $row['firstName'];
+    $lastName = $row['lastName'];
+    $mainSuperpower = $row['superPower'];
+    echo "<article>
+            <h3>{$firstName} {$lastName}</h3>
+            <p>This superhero main power is <strong>{$mainSuperpower}</strong></p>
+          </article>";
+}
+?>
