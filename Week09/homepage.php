810fa040130daacdfe47845334e4079f231e2da8
1511989 <michal.drobena.11@aberdeen.ac.uk>
2016-04-11 10:14:58 +0100

    b

--- Week09/homepage.php
+++ Week09/homepage.php
@@ -5,6 +5,8 @@
  * Date: 11/04/2016
  * Time: 09:39
  */
+$access_level = $_COOKIE['access_level'];
+
 function displayAccessLevelInformation($access_level){
     if($access_level == "standarduser"){
         echo "<p>You are currently logged in as a standard user!</p>";
