12ab45b4daf7c80baf1d52a861451b28e5953327
1511989 <michal.drobena.11@aberdeen.ac.uk>
2016-04-12 09:04:30 +0100

    f

--- Week09/checkLogin.php
+++ Week09/checkLogin.php
@@ -9,9 +9,9 @@ $name = $_POST["name"];
 $password = $_POST["password"];
 
 if($name == "Mike" && $password == "mysecretpassword"){
-    session_start();
-    $_SESSION['access_level'] = "standarduser";
-    /*setcookie('access_level', 'standarduser');*/
+    /*session_start();
+    $_SESSION['access_level'] = "standarduser";*/
+    setcookie('access_level', 'standarduser');
 }
 
 header("Location:homepage.php");
